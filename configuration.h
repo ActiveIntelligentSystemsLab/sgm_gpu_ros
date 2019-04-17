02cb1dcbc1ee344791e2437a9fd1b054a98be9fa
Hironori Fujimoto <fujimoto@aisl.cs.tut.ac.jp>
2019-04-17 16:17:33 +0900

    Remove unused parameters

--- configuration.h
+++ configuration.h
@@ -31,8 +31,6 @@
 #define CENSUS_WIDTH		9
 #define CENSUS_HEIGHT		7
 
-#define OCCLUDED_PIXEL		128
-#define MISMATCHED_PIXEL	129
 
 #define TOP				(CENSUS_HEIGHT-1)/2
 #define LEFT			(CENSUS_WIDTH-1)/2
@@ -40,7 +38,6 @@
 typedef uint32_t cost_t;
 #define MAX_COST		30
 
-#define BLOCK_SIZE					256
 #define COSTAGG_BLOCKSIZE			GPU_THREADS_PER_BLOCK
 #define COSTAGG_BLOCKSIZE_HORIZ		GPU_THREADS_PER_BLOCK
 
